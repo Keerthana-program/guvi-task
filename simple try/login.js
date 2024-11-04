0142377c7530e2ccc07c3ad7cf0d9f707e1c0b89
keerthana-program <keerthanapollachi99@gmail.com>
2024-11-04 20:00:54 +0530

    task 5

---
+++ simple try/login.js
@@ -0,0 +1,5 @@
+const submit = document.getElementById("submitButton");
+submit.addEventListener('click', function() {
+    // Redirect to Google
+    window.location.href = 'https://www.google.com';
+});
